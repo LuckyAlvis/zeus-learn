a9f5b0f83073aea57e5543ba40ed894b140462c2
ivan <252127196@qq.com>
2023-04-08 20:23:47 +0800

    feature: 分模块初始化

---
+++ zeus-learn-server/src/main/java/smart/dai/zeuslearnserver/ZeusLearnServerApplication.java
@@ -0,0 +1,13 @@
+package smart.dai.zeuslearnserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeusLearnServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeusLearnServerApplication.class, args);
+    }
+
+}
---
+++ zeus-learn-server/src/test/java/smart/dai/zeuslearnserver/ZeusLearnServerApplicationTests.java
@@ -0,0 +1,13 @@
+package smart.dai.zeuslearnserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeusLearnServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
