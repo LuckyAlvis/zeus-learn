101dcdc0a28e32b681401eb5ccceedf9367b65af
ivan <252127196@qq.com>
2023-04-08 20:49:07 +0800

    feature: 增加项目启动日志

--- zeus-learn-server/src/main/java/smart/dai/zeuslearnserver/ZeusLearnServerApplication.java
+++ zeus-learn-server/src/main/java/smart/dai/zeuslearnserver/ZeusLearnServerApplication.java
@@ -1,13 +1,20 @@
 package smart.dai.zeuslearnserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import smart.dai.utils.server.ServerUtil;
 
+import java.net.UnknownHostException;
+
+@Slf4j
 @SpringBootApplication
 public class ZeusLearnServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ZeusLearnServerApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(ZeusLearnServerApplication.class, args);
+        log.info(ServerUtil.getAppStartNotifyLog(applicationContext, ZeusLearnServerApplication.class));
     }
 
 }
